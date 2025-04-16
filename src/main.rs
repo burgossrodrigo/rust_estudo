c5caf5289b722b7af9a67523d614c8b3bed0bb31
Rodrigo Burgos <rodrigoburgos@live.com>
2025-04-16 06:45:48 -0300

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
-use rust_estudo::{ding, on_off, print_array, print_difference};
+use std::{
+    sync::{Arc, Mutex, mpsc},
+    thread,
+};
+
+use::rand::Rng;
+
 fn main() {
-    let cords: (f32, f32) = (6.3, 15.0);
+    let (tx, rx) = mpsc::channel::<String>();
 
-    print_difference(cords);
+    let rx = Arc::new(Mutex::new(rx));
 
-    print_array(cords);
+    for workers_id in 1..=3 {
+        let rx_clone = Arc::clone(&rx);
+        thread::spawn(move || {
+            loop {
+                let task = {
+                    let lock = rx_clone.lock().unwrap();
+                    lock.recv()
+                };
 
-    let series = [1, 1, 2, 3, 5, 8, 13];
+                match task {
+                    Ok(task) => {
+                        println!("Worker {} got a task: {}", workers_id, task);
 
-    ding(series);
+                        let delay = rand::thread_rng().gen_range(100..=500);
+                        thread::sleep(std::time::Duration::from_millis(delay));
+                    }
 
-    let mess = ([3, 4], 3.14, [(false, -3), (true, -100)], 5, "candy");
+                    Err(err) => {
+                        println!("Worker {} got a error: {:?}", workers_id, err);
+                        break;
+                    }
+                }
+            }
 
-    on_off(mess.2[1].0)
+            println!("Worker {} is done", workers_id);
+        });
+    }
 
+    for i in 1..=10 {
+        let task = format!("Task {}", i);
+        println!("Main thread sending: {}", task);
+        tx.send(task).unwrap();
+    }
+
+    drop(tx);
+
+    thread::sleep(std::time::Duration::from_secs(3));
+    println!("Main thread is done");
 }
+
+
